bcfb7ce0c39e89a1c9fd21a5d9fbc805bff43b6f
majeurbilly <billy.halle.bh@gmail.com>
2024-07-11 23:26:11 -0400

    deuxieme commit de la premiere journee

---
+++ Projet_14_Salutation_str.format().py
@@ -0,0 +1,6 @@
+nom = "Billy"
+age = 28
+
+phrase = "Salut {}, tu as {} ans.".format(nom, age)
+
+print(phrase)
---
+++ Projet_15_Salutation_Interpolation_de_Chaînes.py
@@ -0,0 +1,6 @@
+nom = "Billy"
+age = 28
+
+phrase = "Salut %s, tu as %d ans." % (nom, age)
+
+print(phrase)
