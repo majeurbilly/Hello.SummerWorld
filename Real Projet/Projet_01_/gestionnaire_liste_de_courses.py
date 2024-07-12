003e3b9a32466a3be98c390ca0998a0ebc01dd4b
c4th4r5y5 <83784862+c4th4r5y5@users.noreply.github.com>
2024-07-12 00:54:46 -0400

    Update gestionnaire_liste_de_courses.py

--- Real Projet/Projet_01_/gestionnaire_liste_de_courses.py
+++ Real Projet/Projet_01_/gestionnaire_liste_de_courses.py
@@ -1,18 +1,26 @@
 ### TOUT en python doit respecter le 'snake_case'
-### donc meme tes noms de fichiers doivent respecter le format : minuscules_tirets_entres_mots
+### donc meme tes noms de fichiers doivent respecter le format : minuscules_tirets_entres_mots.py
 
-def ajouter_article(liste: list[str]):
+# seulement les valeures de type constante (que tu peux garantir que la valeur ne va jamais changer) ont le format TOUTE_EN_MAJUSCULE.
+# seulement les classes (tu est pas rendu la encore) utilise le format 'Capitalization() De() La() Premiere() Lettre()'
+# la raison pourquoi c'est comme ca cest que par default tout est minuscule donc c'est des methodes, si tu voit du code qui commence avec une majuscule tu sait que cest une classe et si tu voit du code qui est toute en majuscule tu sait que cest une constante
+# ex.:  from my_python_file_somewhere import une_methode_qui_multiplie, Tiguan, LA_DATE_DE_LA_ST_JEAN_DE_CETTE_ANNEE
+
+def ajouter_article():
     article = input("Tu veux quoi bitch?")
     liste.apprend(article)
     print(f"{article} a ete rajouter a la liste!")
 
-def afficher_liste(liste: list[str]):
+def afficher_liste():
     print("\nListe de course: ")
     if not liste:
         print("Liste vide!")
     else:
         for i, article in enumerate(liste, 1)
             print(f"{i}. {article}")
+
+def supprimer_article()
+    pass # a faire en devoir
 
 user_input: str | None = None
 liste: list[str] = []
@@ -42,4 +50,4 @@ while user_input != 4:
       case _:
         print(f'yo wtf is : {user_input}?!? Im out!')
         exit(1)
-                                    #À SUIVRE DANS UNE PROCHAINE ÉPISODE
+                                    # À SUIVRE DANS UNE PROCHAINE ÉPISODE
