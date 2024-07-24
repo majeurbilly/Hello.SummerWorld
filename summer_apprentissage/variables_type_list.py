12e67f68b3de801f9ea9dd1ff8e7be9b72069651
majeurbilly <billy.halle.bh@gmail.com>
2024-07-24 14:54:24 -0400

    cours_0.2_0_2_14h53

--- summer_apprentissage/variables_type_list.py
+++ summer_apprentissage/variables_type_list.py
@@ -1,9 +1,10 @@
 
+# TJS conscidérer que c'est 0, 1 , 2, 3 ... etc
+
 
 nombres = [1, 2, 3, 4, 5]  #todo
 fruits = ["pomme", "banane", "cerise"]
 animaux = ["chien", "chat", "humain", "licorne"]
-# TJS conscidérer que c'est 0, 1 , 2, 3 ... etc
 nombres[0] = 10   #todo Wtf pas encore compris
 
 print(nombres[0]) #todo
