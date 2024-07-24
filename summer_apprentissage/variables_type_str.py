ec69c1d6162432b25004628d12e7ef62000a5e56
majeurbilly <billy.halle.bh@gmail.com>
2024-07-24 14:36:43 -0400

    cours_0.2_0_2_14h36

---
+++ summer_apprentissage/variables_type_str.py
@@ -0,0 +1,23 @@
+
+
+#Variable : Un conteneur nommé pour stocker des données.
+
+#Variables :
+x = 4
+y = "Billy"
+a = 4
+b = "le plus cool"
+c = ", je pense que j'ai saisis le priciple des variable."
+d = "Next!!"
+e = "Stu " + "quelques " + "chose " + "de " + "fessable" + "?"
+f = "Stu" + " " + "quelques" + " " + "chose" + " " + "de" + " " + "fessable" + "?"
+g = " ".join(["Stu", "quelques", "chose", "de", "fessable?"])  #Wtf ca, ca brisé mon cerveau
+
+print(x)
+print(y)
+print(x + a)
+print(y, b)
+print(y, b + c + d)
+print(e)
+print(f)
+print(g)
