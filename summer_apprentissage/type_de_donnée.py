43569870af75493cfe7347e51a0285817702066c
majeurbilly <billy.halle.bh@gmail.com>
2024-07-24 14:37:09 -0400

    cours_0.2_0_2_14h36

---
+++ summer_apprentissage/type_de_donnée.py
@@ -0,0 +1,50 @@
+
+#Variable : Un conteneur nommé pour stocker des données.
+#Type de Données : La nature de la valeur contenue dans la variable.
+
+
+#int (nombre entier) :
+age = 25
+pomme = 72
+jours = 365
+dette de mouton = -6
+
+
+#float (nombre décimal) :
+pi = 3.14
+temperature = -10.5
+prix du gaz = 1.83
+
+
+#str (chaîne de caractères) :
+greeting = "Salutation"
+nom = "Billy"
+age = "69"
+f = "Stu" + " " + "quelques" + " " + "chose" + " " + "de" + " " + "fessable" + "?"
+g = " ".join(["Stu", "quelques", "chose", "de", "fessable?"])
+
+
+#bool (booléen ; verifie les conditions) :
+lumiere.allumee = True
+lumiere.fermee = False
+utilisateur_connecte = True
+utilisateur_deconnecte = False
+
+
+#list (liste ; collection ordonné modifiable) :
+numbers = [1, 2, 3, 4]
+fruits = ["pomme", "banane", "cerise"]
+
+
+#tuple (tuple ; collection ordonné immuable *non modifialbe) :
+coordinates = (10, 20)
+
+
+#dict (dictionnaire ; collection paire, clé-valeur) :
+student = {"name": "Alice", "age": 25}
+
+
+#set (ensemble ; valeur unique non ordonné) :
+unique_numbers = {1, 2, 3, 4}
+
+
