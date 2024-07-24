a123df85926db3f4736ecab4ef5ea7b86be0b5d2
majeurbilly <billy.halle.bh@gmail.com>
2024-07-24 14:53:16 -0400

    cours_0.2_0_2_14h53

--- summer_apprentissage/variables_type_list.py
+++ summer_apprentissage/variables_type_list.py
@@ -1,11 +1,14 @@
 
 
-nombres = [1, 2, 3, 4, 5]
+nombres = [1, 2, 3, 4, 5]  #todo
 fruits = ["pomme", "banane", "cerise"]
+animaux = ["chien", "chat", "humain", "licorne"]
+# TJS conscidérer que c'est 0, 1 , 2, 3 ... etc
+nombres[0] = 10   #todo Wtf pas encore compris
 
-
-print(nombres[0])
+print(nombres[0]) #todo
 print(fruits[1])
-
+print(animaux[3])
+print(nombres) #todo
 
 
