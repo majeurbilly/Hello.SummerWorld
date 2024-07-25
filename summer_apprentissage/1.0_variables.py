1f66e2b826de045ed585013db5c768161550828a
majeurbilly <billy.halle.bh@gmail.com>
2024-07-25 13:26:29 -0400

    cours_0.2_13h26

--- summer_apprentissage/1.0_variables.py
+++ summer_apprentissage/1.0_variables.py
@@ -4,9 +4,16 @@ y = 3.14
 est_vrai = True
 ma_liste_de_chiffre = [1, 2, 3, 4, 5]
 ma_liste_de_mot = ["moi", "plus toi", "plus zoboumafou"]
-affichage = " ".join(ma_liste_de_mot)
-
-
+liste_en_phrase = " ".join(ma_liste_de_mot)
+mon_tuple = (10, 20, 30)
+mon_dict = {"nom": "billy", "binaritée": 1}
+mon_set = {1, 2, 3, 4, 5}
+valeur_1 = None
+valeur_2 = True
+valeur_3 = False
+octets = b"commprend pas encore"
+tableau_octets = bytearray(b"wtf")
+vue = memoryview(b"wtfx1000")
 
 
 print(x)
@@ -15,5 +22,13 @@ print(y)
 print(est_vrai)
 print(ma_liste_de_chiffre)
 print(ma_liste_de_mot)
-print(affichage)
-
+print(liste_en_phrase)
+print(mon_tuple)
+print(mon_dict)
+print(mon_set)
+print(valeur_1)
+print(valeur_2)
+print(valeur_3)
+print(octets)
+print(tableau_octets)
+print(vue)
